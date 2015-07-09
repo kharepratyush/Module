77dd477cc74d65779c77b723b54fc909783fe54b
kharepratyush <khare.pratyush@outlook.com>
2015-07-09 12:48:44 +0530

    Finishing touch :-)

--- routes/forum.js
+++ routes/forum.js
@@ -24,24 +24,24 @@ onFileUploadComplete: function (file) {
 }));
 
 
-AWS.config.update({ accessKeyId: 'XXXXXXXXXXXXXXXXXXXXXX', secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' });
+AWS.config.update({ accessKeyId: 'XXXXXXXXXXXXXX', secretAccessKey: 'XXXXXXXXXXXXXX' });
 
 var connection = mysql.createConnection({
 	host : 'localhost',
-	user : 'XXXXXXXXXX',
-	password : 'XXXXXXXXXX',
-	database : 'XXXXXXXXXXX',
+	user : 'dezyre_user',
+	password : 'omair123!',
+	database : 'dezyre_db',
 	table : 'project_question'
 });
 
 var app = express();
 
 connection.connect(function(err){
-if(!err) {
-    console.log("Forum connected to database");  
-} else {
-    console.log(err);  
-}
+	if(!err) {
+	    console.log("Forum connected to database");  
+	} else {
+	    console.log(err);  
+	}
 });
 
 router.get('/', function(req,res){
@@ -128,32 +128,13 @@ router.post('/uploadData/', function(req, res, next)
 	});
 
 });
-/*
-router.post('/', function(req, res, next) 
-{
-	console.log(req.body);
 
-	var input = JSON.parse(JSON.stringify(req.body));
-	var data = {
-				question : input.question,
-				user_id: input.user_id,
-				project_id : input.project_id
-				};
-
-	connection.query("INSERT INTO project_question SET ?", data, function(err,fields){
-		  if (err)
-              console.log("Error inserting : %s ",err );
-     	});
-	res.redirect('/forum');
-
-});
-*/
 router.get('/question/:id', function(req, res){
 
 	var id = req.params.id;
 	
 	console.log(id);
-	connection.query('SELECT question,vote_count FROM project_question WHERE project_question_id = ?', [id],function(err,fields){
+	connection.query('SELECT question,vote_count,project_question_id FROM project_question WHERE project_question_id = ?', [id],function(err,fields){
 		if(err)
 			console.log("Error in getting question: %s ", err);
 
@@ -168,7 +149,7 @@ router.get('/question/:id', function(req, res){
 			console.log("Error in counting answers: %s", err);
 		count_data = fields;
 	});
-	connection.query('SELECT answer,vote_count,project_answer_id,user_id FROM project_answer WHERE project_question_id = ?',[id], function(err, fields){
+	connection.query('SELECT answer,vote_count,project_answer_id,user_id,aws_path FROM project_answer WHERE project_question_id = ?',[id], function(err, fields){
 		if(err)
 			console.log("Error in getting answers: %s" ,err);
 		ans_data = fields;
@@ -204,6 +185,61 @@ router.post('/question/:id', function(req, res, next)
 
 });
 
+router.post('/question/:id/answer/uploadfile/', function(req, res, next) 
+{
+	//console.log(req.body.question)
+	if(done)
+	{
+		var location;
+		var id = req.params.id;
+		console.log(req.files);
+		if(req.files.file_upload)
+		{
+
+			console.log(req.files.file_upload);
+
+			fs.readFile(path.join(__dirname ,"../",req.files.file_upload.path), function (err, data) {
+		 
+				if (err) { throw err; }
+
+			  	var s3 = new AWS.S3();
+			  	s3.upload({
+				    Bucket: 'question.project.dezyre',
+				    Key: req.files.file_upload.name,
+				    Body: data
+			  	}).on('httpUploadProgress', function(evt) { /*console.log(evt);*/ }).send(function(err, data1) 
+				{ 
+			  		if(err) console.log(err); 
+
+
+				  	else 
+				  	{	
+				  		
+
+				  	 	console.log(data1.Location); 		
+				  	 	var input = JSON.parse(JSON.stringify(req.body));
+						console.log(input);
+						var data = {
+									answer : input.answer,
+									project_question_id:id,
+									user_id : input.user_id,
+									aws_path : data1.Location
+									};
+
+						connection.query("INSERT INTO project_answer SET ?", data, function(err,fields){
+						  if (err)
+				              console.log("Error inserting : %s ",err );
+				     	});
+					}
+			     	
+				});
+			});
+
+			fs.unlink(path.join(__dirname ,"../",req.files.file_upload.path),function(err){if(err)console.log(err)});
+			res.redirect('/forum/question/'+ id);
+		} }
+});
+	
 
 
 module.exports = router;
